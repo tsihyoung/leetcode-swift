655d9107cb264f9c8ef16d9726bec331b4346805
Young WU <tsihyoung@gmail.com>
2018-10-24 22:08:45 +0800

    update

---
+++ problem171.swift
@@ -0,0 +1,33 @@
+class Solution1 {
+    func pow(_ power: Int) -> Int {
+        var result = 1
+        for time in 0..<power {
+            result *= 26
+        }
+        return result
+    }
+    func titleToNumber(_ s: String) -> Int {
+        let s = s.unicodeScalars
+        var power = s.count - 1
+        var result = 0
+        
+        for char in s {
+            result += (Int(char.value) - 64)*pow(power)
+            power -= 1
+        }
+        return result
+    }
+}
+
+class Solution2 {
+    func titleToNumber(_ s: String) -> Int {
+        let s = s.unicodeScalars
+        var result = 0
+        
+        for char in s {
+            result *= 26
+            result += (Int(char.value) - 64)
+        }
+        return result
+    }
+}
